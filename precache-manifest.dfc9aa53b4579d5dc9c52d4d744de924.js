22d9951bb9d65dfc365f563f30847410b54d2c14
Jamen1147 <jamen1147@gmail.com>
2019-09-30 21:24:14 +1000

    Updates

---
+++ precache-manifest.dfc9aa53b4579d5dc9c52d4d744de924.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1954079b45cd40965d8d2a5ce81cda",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "d6d01dca503ff2939e80",
+    "url": "/todolist/static/css/main.df7f367c.chunk.css"
+  },
+  {
+    "revision": "1939a737a404f27a3f0d",
+    "url": "/todolist/static/js/2.49d6e578.chunk.js"
+  },
+  {
+    "revision": "d6d01dca503ff2939e80",
+    "url": "/todolist/static/js/main.43d68d56.chunk.js"
+  },
+  {
+    "revision": "71c9dd1659af9b7925b1",
+    "url": "/todolist/static/js/runtime-main.db5ab4e8.js"
+  }
+]);
